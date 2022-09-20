c59c19cdc5e276848bc9fdd5f8bfa2d3af87e894
Yuuki915 <aso091335@gmail.com>
2022-09-20 15:26:43 -0700

    modify route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App";
-import FoodPage from "./components/pages/FoodPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/food" element={<FoodPage />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
