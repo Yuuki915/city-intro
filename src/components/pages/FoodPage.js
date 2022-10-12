48b9a7fd7916224bd8f97fd72fbca36e12efd821
Yuuki915 <aso091335@gmail.com>
2022-10-12 13:29:40 -0700

    modify maps

--- src/components/pages/FoodPage.js
+++ src/components/pages/FoodPage.js
@@ -1,38 +1,39 @@
 import React from "react";
-import { useLoadScript } from "@react-google-maps/api";
+// import { useLoadScript } from "@react-google-maps/api";
 import Map from "./Map";
 import Header from "../partials/Header";
 import Footer from "../partials/Footer";
 
 import "../css/FoodPage.css";
 
-// why making const is to avoid too many re-renders
 // const libraries = ["places"];
 
 const FoodPage = ({ foodItem }) => {
-  const { isLoaded } = useLoadScript({
-    id: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    // libraries,
-  });
-  if (!isLoaded) return <div>Loading...</div>;
+  // const { isLoaded } = useLoadScript({
+  //   id: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  //   // libraries,
+  // });
+  // if (!isLoaded) return <div>Loading...</div>;
 
   const shops = foodItem.shops;
   return (
     <div className="food-page">
       <Header />
-      <div>{foodItem.foodTitle}</div>
-      <Map latLng={foodItem.shops} />
+      <div className="food-page-main">
+        <div>{foodItem.foodTitle}</div>
+        <Map latLng={foodItem.shops} />
 
-      <div>
-        <h3>Popular in Town</h3>
         <div>
-          {shops.map((item, key) => (
-            <div key={key}>
-              <p>{item.name}</p>
-              <p>{item.memo}</p>
-            </div>
-          ))}
+          <h3>Popular in Town</h3>
+          <div>
+            {shops.map((item, key) => (
+              <div key={key}>
+                <p>{item.name}</p>
+                <p>{item.memo}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
