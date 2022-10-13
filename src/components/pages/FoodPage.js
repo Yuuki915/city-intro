9ca426f304f4e88962def49fcd565c0cc7976567
Yuuki915 <aso091335@gmail.com>
2022-10-13 04:19:03 -0700

    modify layout, foodpage

--- src/components/pages/FoodPage.js
+++ src/components/pages/FoodPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Map from "./Map";
 import Header from "../partials/Header";
 import Footer from "../partials/Footer";
@@ -6,28 +6,55 @@ import Footer from "../partials/Footer";
 import "../css/FoodPage.css";
 
 const FoodPage = ({ foodItem }) => {
+  const [clickedShop, setClickedShop] = useState("");
+  console.log(clickedShop);
   const shops = foodItem.shops;
   const memo = foodItem.memo;
-  console.log(shops);
+  console.log(foodItem);
 
   return (
     <div className="food-page">
       <Header />
       <div className="food-page-main">
-        <div>{foodItem.foodTitle}</div>
-        <Map latLng={foodItem.shops} />
+        <div className="memo">
+          <h3>Memo</h3>
+          <p>{memo}</p>
+        </div>
+
+        <div className="title-container">
+          <h1 className="food-title">{foodItem.foodTitle}</h1>
+          <p className="title-border"></p>
+        </div>
+
+        <Map foodItem={foodItem} setClickedShop={setClickedShop} />
 
         <div className="shops-container">
-          <h2 className="foodpage-h2">Popular in Town</h2>
+          <div className="title-container">
+            <h2 className="foodpage-h2">Popular in Town</h2>
+            <p className="title-border"></p>
+          </div>
+
           <div className="shops">
             {shops.map((item, key) => (
-              <div key={key} className="shop">
-                <p className="shop-name">{item.name}</p>
-                <div className="adds">
-                  <p className="address-en">{item.addressEn}</p>
-                  <p className="address-ja">{item.addressJa}</p>
-                </div>
-              </div>
+              <>
+                {clickedShop === item ? (
+                  <div key={key} className="shop shop-chosen">
+                    <p className="shop-name">{item.name}</p>
+                    <div className="adds">
+                      <p className="address-en">{item.addressEn}</p>
+                      <p className="address-ja">{item.addressJa}</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div key={key} className="shop">
+                    <p className="shop-name">{item.name}</p>
+                    <div className="adds">
+                      <p className="address-en">{item.addressEn}</p>
+                      <p className="address-ja">{item.addressJa}</p>
+                    </div>
+                  </div>
+                )}
+              </>
             ))}
           </div>
         </div>
