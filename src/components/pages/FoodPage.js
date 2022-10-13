51d1d3cc88a57e0362f33cb2828d509b819e74ee
Yuuki915 <aso091335@gmail.com>
2022-10-13 01:20:27 -0700

    modify foodpage

--- src/components/pages/FoodPage.js
+++ src/components/pages/FoodPage.js
@@ -1,22 +1,15 @@
 import React from "react";
-// import { useLoadScript } from "@react-google-maps/api";
 import Map from "./Map";
 import Header from "../partials/Header";
 import Footer from "../partials/Footer";
 
 import "../css/FoodPage.css";
 
-// const libraries = ["places"];
-
 const FoodPage = ({ foodItem }) => {
-  // const { isLoaded } = useLoadScript({
-  //   id: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  //   googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  //   // libraries,
-  // });
-  // if (!isLoaded) return <div>Loading...</div>;
-
   const shops = foodItem.shops;
+  const memo = foodItem.memo;
+  console.log(shops);
+
   return (
     <div className="food-page">
       <Header />
@@ -24,13 +17,16 @@ const FoodPage = ({ foodItem }) => {
         <div>{foodItem.foodTitle}</div>
         <Map latLng={foodItem.shops} />
 
-        <div>
-          <h3>Popular in Town</h3>
-          <div>
+        <div className="shops-container">
+          <h2 className="foodpage-h2">Popular in Town</h2>
+          <div className="shops">
             {shops.map((item, key) => (
-              <div key={key}>
-                <p>{item.name}</p>
-                <p>{item.memo}</p>
+              <div key={key} className="shop">
+                <p className="shop-name">{item.name}</p>
+                <div className="adds">
+                  <p className="address-en">{item.addressEn}</p>
+                  <p className="address-ja">{item.addressJa}</p>
+                </div>
               </div>
             ))}
           </div>
